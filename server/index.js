9d8a3910fcc5c31cc17143caff894dce523d3938
chetan09upadhyay <132812698+chetan09upadhyay@users.noreply.github.com>
2023-08-21 18:10:18 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://talky-chats-app.vercel.app"],
+        methods: ["POST","GET"],
+        credentials: true
+    }    
+));
 app.use(express.json());
 
 
